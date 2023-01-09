df342548cf84bd28415bcbbbc31e4963548654c9
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-01-09 12:42:59 -0500

    Add 5
    Add to Map

--- Map.java
+++ Map.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
+import java.io.*;
 
 public class Map {
-    private final ArrayList<Room> rooms;
+    private final String name;
+    private ArrayList<Room> rooms;
+    private BufferedReader input;
+
+    Map(File mapFile) throws Exception{
+        input = new BufferedReader(new FileReader(mapFile));
+        this.name = input.readLine();
+    }
 }
