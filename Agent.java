7985fe2b7d51dfdc14bfc46908eb154883a5ff05
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-01-10 23:04:14 -0500

    Add 6.5.5
    Added Agents

--- Agent.java
+++ Agent.java
@@ -1,10 +1,16 @@
 public enum Agent {
-    /* TODO: implement agents */;
+    ZERO(0, 1, 0),
+    MUSTANG(1, 2, 0),
+    HELLCAT(2, 3, 0),
+    WILDCAT(3, 4, 0),
+    MIRAGE(4, 5, 0);
+
+//-------------------------------------------------------------------------------------
     private final int agentId;
     private final int basicAbility; // ability Id
     private final int ultimateAbility; // ability Id
 
-    Agent(int agentId, String imagePath, int basicAbility, int ultimateAbility) {
+    Agent(int agentId, int basicAbility, int ultimateAbility) {
         this.agentId = agentId;
         this.basicAbility = basicAbility;
         this.ultimateAbility = ultimateAbility;
