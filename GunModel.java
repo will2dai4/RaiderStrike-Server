0f22c352125ec52cb8e6580d576e689865e7a35a
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-01-10 19:33:48 -0500

    Add 6.5
    Added to GunModel

--- GunModel.java
+++ GunModel.java
@@ -1,16 +1,23 @@
 public enum GunModel {
     /* TODO: implement gun models */;
     // Name of enum constant can be acquired using .toString() method
+    private final int damage;
     private final int maxAmmo;
     private final double reloadSpeed;
     private final double fireRate;
+    private final double fireError;
 
-    private GunModel(int maxAmmo, double reloadSpeed, double fireRate) {
+    private GunModel(int damage, int maxAmmo, double reloadSpeed, double fireRate, double fireError) {
+        this.damage = damage;
         this.maxAmmo = maxAmmo;
         this.reloadSpeed = reloadSpeed;
         this.fireRate = fireRate;
+        this.fireError = fireError;
     }
-    
+
+    public int getDamage(){
+        return this.damage;
+    }
     public int getMaxAmmo() {
         return this.maxAmmo;
     }
@@ -19,5 +26,8 @@ public enum GunModel {
     }
     public double getFireRate() {
         return this.fireRate;
+    }
+    public double getFireError(){
+        return this.fireError;
     }
 }
--- Server.java
+++ Server.java
@@ -12,4 +12,8 @@ public class Server {
     public static void main(String[] args){
 
     }
+
+    class ThreadMachine{
+        
+    }
 }
