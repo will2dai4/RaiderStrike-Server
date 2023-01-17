56c6ff29cbf2a067c202c0ca62dbf07782e1a158
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-01-16 19:21:16 -0500

    Add + Edit 14
    Remodeled Server and Player (PlayerHanlder is now apart of Player instead of Server)

--- Player.java
+++ Player.java
@@ -1,7 +1,11 @@
-public class Player extends GameObject{
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class Player extends GameObject implements Runnable {
     private String name;
     private int team; // 0 -> Red Team | 1 -> Blue Team
-    private int playerId; 
+    private int playerId;
     private Agent agent;
     private boolean ready;
 
@@ -12,135 +16,245 @@ public class Player extends GameObject{
     private int health;
     private boolean alive;
     private int numCredits;
-    private boolean hasSpike;
+    private boolean hasBomb;
     private double movementSpeed;
     private final double defaultMovementSpeed;
     private int direction; // degrees
 
-    Player(int newPlayerId){
-        this.playerId = newPlayerId;
-        
+    Socket socket;
+    PrintWriter output;
+    BufferedReader input;
+
+    Queue<String> messages;
+
+    Player(int playerId, Socket socket) throws IOException {
+        this.playerId = playerId;
+        this.socket = socket;
+
         this.team = -1;
         this.holdingSlot = 2;
         this.defaultMovementSpeed = Const.PLAYER_MOVEMENT_SPEED;
         this.movementSpeed = defaultMovementSpeed;
+
+        this.messages = new LinkedList<String>();
+
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.output = new PrintWriter(socket.getOutputStream());
     }
 
     public boolean collides(GameObject other) {
         return false;
     }
-    public void resetMovementSpeed(){
+
+    public void resetMovementSpeed() {
         this.movementSpeed = defaultMovementSpeed;
     }
 
-//------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------
+
+    public void kill() {
+        Gun gunDrop;
+        if (this.getPrimGun() != null) {
+            gunDrop = this.getPrimGun();
+        } else {
+            gunDrop = this.getSecGun();
+        }
+        this.setAlive(false);
+        /* TODO: Drop gun function */
+    }
+
+    // Client to server commands
+    public void name(String name) {
+        this.setName(name);
+    }
+
+    public void update(){
+        while(!messages.isEmpty()){
+            String msg = messages.poll();
+            String[] args = msg.split(" ", 2);
+            String command = args[0];
+            switch(command){
+                case "ERROR":
+                case "NAME":
+                case "TEAM":
+                case "AGENT":
+                case "READY":
+            }
+        }
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                String msg = input.readLine();
+                if (msg.length() > 0) {
+                    System.out.println("input: " + msg);
+                    this.messages.add(msg);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void print(String text) {
+        if (this.socket == null) {
+            System.out.println("Dead socket, message send failure");
+            return;
+        }
+        this.output.println(text);
+        this.output.flush();
+    }
+
+    // ------------------------------------------------------------------------------------------------
     // getters and setters
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
-    public int getTeam(){
+
+    public int getTeam() {
         return this.team;
     }
-    public int getPlayerId(){
+
+    public int getPlayerId() {
         return this.playerId;
     }
-    public Agent getAgent(){
+
+    public Agent getAgent() {
         return this.agent;
     }
-    public boolean checkReady(){
+
+    public boolean checkReady() {
         return this.ready;
     }
-    public int getCredits(){
+
+    public int getCredits() {
         return this.numCredits;
     }
-    public boolean getAlive(){
+
+    public boolean getAlive() {
         return this.alive;
     }
-    public Gun getPrimGun(){
+
+    public Gun getPrimGun() {
         return this.primaryGun;
     }
-    public Gun getSecGun(){
+
+    public Gun getSecGun() {
         return this.secondaryGun;
     }
-    public int getHealth(){
+
+    public int getHealth() {
         return this.health;
     }
-    public double getMovementSpeed(){
+
+    public double getMovementSpeed() {
         return this.movementSpeed;
     }
-    public boolean checkSpike(){
-        return this.hasSpike;
+
+    public boolean checkSpike() {
+        return this.hasBomb;
     }
-    public int getDirection(){
+
+    public int getDirection() {
         return this.direction;
     }
-    public Gun getHolding(){
-        if(this.holdingSlot == 1) return primaryGun;
-        if(this.holdingSlot == 2) return secondaryGun;
-        else return null;
+
+    public Gun getHolding() {
+        if (this.holdingSlot == 1)
+            return primaryGun;
+        if (this.holdingSlot == 2)
+            return secondaryGun;
+        else
+            return null;
     }
-    public int getItemsHolding(){
+
+    public int getItemsHolding() {
         return this.itemsHolding;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
-    public void setTeam(int teamId){
+
+    public void setTeam(int teamId) {
         this.team = teamId;
     }
-    public void setAgent(String agentName){
+
+    public void setAgent(String agentName) {
         this.agent = Agent.valueOf(agentName);
     }
-    public void setReady(){
+
+    public void setReady() {
         this.ready = !(this.ready);
     }
-    public void setCredits(int credits){
+
+    public void setCredits(int credits) {
         this.numCredits = credits;
     }
-    public void setAlive(boolean alive){
+
+    public void setAlive(boolean alive) {
         this.alive = alive;
     }
-    public void setGun(int slot, Gun gun){
-        switch(slot){
-            case 0: this.primaryGun = gun;
-            case 1: this.secondaryGun = gun;
+
+    public void setGun(int slot, Gun gun) {
+        switch (slot) {
+            case 0:
+                this.primaryGun = gun;
+            case 1:
+                this.secondaryGun = gun;
         }
     }
-    public void setMovementSpeed(double newSpeed){
+
+    public void setMovementSpeed(double newSpeed) {
         this.movementSpeed = newSpeed;
     }
-    public void setHealth(int health){
+
+    public void setHealth(int health) {
         this.health = health;
     }
-    public void setHealth(boolean damage, int amount){
-        if(damage) this.health -= amount;
-        if(!damage) this.health += amount;
+
+    public void setHealth(boolean damage, int amount) {
+        if (damage)
+            this.health -= amount;
+        if (!damage)
+            this.health += amount;
     }
-    public void setSpike(boolean hasSpike){
-        this.hasSpike = hasSpike;
+
+    public void setSpike(boolean hasSpike) {
+        this.hasBomb = hasSpike;
     }
-    public void setDirection(int degrees){
+
+    public void setDirection(int degrees) {
         this.direction = degrees % 360;
     }
-    public void setDirection(int deltaX, int deltaY){
-        this.direction = (int)(Math.atan2(deltaY, deltaX));
+
+    public void setDirection(int deltaX, int deltaY) {
+        this.direction = (int) (Math.atan2(deltaY, deltaX));
     }
-    public void setHolding(int gunSlot){
-        switch(gunSlot){
-            case 1: this.holdingSlot = gunSlot;
-            case 2: this.holdingSlot = gunSlot;
+
+    public void setHolding(int gunSlot) {
+        switch (gunSlot) {
+            case 1:
+                this.holdingSlot = gunSlot;
+            case 2:
+                this.holdingSlot = gunSlot;
         }
     }
-    public void switchWeaponUp(){
-        this.holdingSlot = (this.holdingSlot+1) % itemsHolding;
+
+    public void switchWeaponUp() {
+        this.holdingSlot = (this.holdingSlot + 1) % itemsHolding;
     }
-    public void switchWeaponDown(){
-        if(this.holdingSlot == 1) this.holdingSlot = this.itemsHolding;
-        else this.holdingSlot--;
+
+    public void switchWeaponDown() {
+        if (this.holdingSlot == 1)
+            this.holdingSlot = this.itemsHolding;
+        else
+            this.holdingSlot--;
     }
-    public void setItemsHolding(int numItems){
+
+    public void setItemsHolding(int numItems) {
         this.itemsHolding = numItems;
     }
 }
--- Server.java
+++ Server.java
@@ -8,15 +8,11 @@ public class Server {
     PrintWriter output;
     BufferedReader input;
     int clientCounter;
-    int idCounter = Integer.MIN_VALUE;
 
     StateMachine state; /* TODO: implement */
-    HashSet<PlayerHandler> handlers;
     HashMap<Integer, Player> players;
 
-    HashMap<Integer, Player> handlerMap;
-
-    public final Object playerLock = new Object();
+    ArrayList<Thread> playerThreads;
 
     public void go() throws IOException{
         System.out.println("Starting Server...");
@@ -26,98 +22,24 @@ public class Server {
 
         while(true){
             clientSocket = serverSocket.accept();
-            clientCounter++;
             System.out.println(clientCounter + " clients connected.");
-            PlayerHandler playerHandler = new PlayerHandler(clientSocket);
-            handlers.add(playerHandler);
-            playerHandler.start();
-            synchronized (playerLock) {
-                for (Integer id: this.players.keySet()) {
-                    System.out.println("ID " + id); // Gives client its id
-                }
+            Player player = new Player(clientCounter, clientSocket);
+            players.put(player.getPlayerId(), player);
+            Thread playerThread = new Thread(player);
+            playerThreads.add(playerThread);
+            playerThread.start();
+            player.print("ID " + player.getPlayerId());
+            for (Integer id: this.players.keySet()) {
+                player.print("PLAYER " + id); // Gives client its id
             }
         }
     }
     public void setUp(){
         this.state = StateMachine.MenuState;
-        this.handlers = new HashSet<>();
         this.players = new HashMap<>();
-        this.handlerMap = new HashMap<>();
+        this.playerThreads = new ArrayList<Thread>();
     }
 
-    public Player createPlayer(PlayerHandler playerHandler){
-        Player player = new Player(idCounter++);
-        synchronized(playerLock){
-            this.players.put(player.getPlayerId(), player);
-        }
-        this.handlerMap.put(player.getPlayerId(), player);
-        playerHandler.print("PLAYER " + player.getPlayerId()); // Gives client player ID
-        return player;
-    }
-
-    class PlayerHandler extends Thread{
-        Socket socket;
-        PrintWriter output;
-        BufferedReader input;
-        Player player;
-        PlayerHandler(Socket socket){
-            this.socket = socket;
-        }
-
-        public void inputName(String name){
-            player.setName(name);
-        }
-
-        public void kill(){
-            Gun gunDrop;
-            if(player.getPrimGun() != null){
-                gunDrop = this.player.getPrimGun();
-            } else {
-                gunDrop = this.player.getSecGun();
-            }
-            this.player.setAlive(false);
-            /* TODO: Drop gun function */
-        }
-
-        public void run(){
-            try {
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                output = new PrintWriter(socket.getOutputStream());
-                while(true){
-                    String msg = input.readLine();
-                    if(msg.length() > 0){
-                        System.out.println("input: " + msg);
-                        msg = state.validInput(msg); // checks if the presented command is possible
-                        String[] args = msg.split(" ", 2);
-                        String command = args[0];
-                        try {
-                            switch(command){
-                                case "ERROR":
-                                case "NAME":
-                                case "TEAM":
-                                case "AGENT":
-                                case "READY":
-                                /* TODO: fill in conditions */
-                            }
-                        } catch (Exception e) {
-                            /* TODO: Error message */
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void print(String text) {
-            if (this.socket == null) {
-                System.out.println("Dead socket, message send failure");
-                return;
-            };
-            output.println(text);
-            output.flush();
-        }
-    }
     class PlayerThread extends Thread{
 
     }
